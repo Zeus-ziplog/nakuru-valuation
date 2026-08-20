5aec73a76f04513d4df292f8233c0bc6e480c653
Zeus-ziplog <ziplogziki@gmail.com>
2026-08-20 16:15:53 +0300

    Add outlier detection, shapefile export, and PWA support

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'nakuru-info-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/nakuru_wards.geojson',
+  '/nakuru_subcounties.geojson',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  // Add other static assets you want cached (e.g., your icons)
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
